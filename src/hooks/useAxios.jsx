963791de85b3b10a7ca47302e66762ec50dbc1b2
chegarty74 <chegarty74@yahoo.com>
2022-07-21 12:08:13 -0500

    Finish Login and Register pages, start Axios build

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+
+function useAxios(url){
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    
+
+
+
+
+    return(
+        <div>DONT DO IT EMMANUEL!!!!!</div>
+    )
+}
+
+export default useAxios
