d13c57a7ffb512d29267362f744f9c676de94efb
chegarty74 <chegarty74@yahoo.com>
2022-07-21 14:56:28 -0500

    Set up Axios hook

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,19 +1,31 @@
 import React, { useState, useEffect } from "react";
-import Axios from "axios";
+import axios from "axios";
+
+const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=Kljl21HbtxWMTX68EbeomPuzwqVuvu7e&limit=100&offset=0&lang=en&rating=";
 
 function useAxios(url){
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
+    useEffect(() => {
+        async function init(){
+            setLoading(true);
+            setError(null);
+            setData(null);
+            try {
+                const response = await axios.get(baseUrl + url);
+                setData(response.data)
+            } catch(e) {
+                setError(e);
+            } finally {
+                setLoading(false)
+            }
+        }
+        init();
+    },[url])
     
-
-
-
-
-    return(
-        <div>DONT DO IT EMMANUEL!!!!!</div>
-    )
+    return {data, error, loading}
 }
 
 export default useAxios
