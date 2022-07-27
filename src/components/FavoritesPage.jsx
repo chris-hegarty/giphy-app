1c979c2fb6f8f5ebc14a76675d8b488148e37ee4
chegarty74 <chegarty74@yahoo.com>
2022-07-26 20:36:35 -0500

    Begin Favorite Page

--- src/components/FavoritesPage.jsx
+++ src/components/FavoritesPage.jsx
@@ -1,44 +1,32 @@
 import React, {useState, useContext} from "react";
 // import { NavLink, useNavigate } from "react-router-dom";
 import {FavoritesContext} from "../context/FavoritesContext";
-import { UserContext } from "../context/UserContext";
 // import useAxios from "../hooks/useAxios"
-import { FaHeart } from 'react-icons/fa';
-
+import SingleGif from "../components/SingleGif"
 
 //an array to contain the favorites has been created with state in Favorites context.
 // const [favorites, setFavorites]= useState([]);
 //Favorites context with the functions to change the array is imported above.
 
-
 function FavoritesPage(props){
     const {favorites, add, remove}= useContext(FavoritesContext);
-    const loggedInUser = useContext(UserContext)
 
     return(
         <>
         <div className="parent-section flex flex-wrap">
-            {/* {data && data.map((data, idx) => (
-            <div key={idx}>
-                <img
-                    onClick={(data) => (data)}
-                    src={data.url}
-                    alt={data.title}
-                />
-                {!isFavorite &&
+                {favorites &&
+                    favorites.length > 0 &&
+                    favorites.map((val) => (
+                        <SingleGif
+                            gif={val}
+                            add={add}
+                            remove={remove}
+                            key={val.gif_id}
+                        />
+                    )
+                    )
+                }
 
-                <button
-                    className="favorite-icon"
-                    onClick={((e) => {
-
-                    }
-                    )}
-                >
-                    < FaHeart />
-                </button>
-                }           
-            </div>
-            ))} */}
         </div>
         </>
     )
