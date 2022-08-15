cac11ccc5b482798df1776d71052733cd7f181f3
chegarty74 <chegarty74@yahoo.com>
2022-08-14 20:21:55 -0500

    Setup database config, being routing files.

---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config;
+const userRoutes = require("./server/routes/users.routes");
+const favoritesRoutes = require("./server/routes/favorites.routes");
+const passport = require("./server/config/passport.config");
+const cookieParser = require("cookie-parser");
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT ?? 8080;
+
+app.use(express.json());
+app.use(express.static(__dirname) + "/build");
+//* Implement passport and cookie-parser.
+//* Cookie parser needs to be first.
+app.use(cookieParser());
+app.use(passport.initialize());
+//*Routes:
+app.use("/api/favorites", favoritesRoutes);
+app.use("/api/users", userRoutes);
+
+app.get("*", (req, res) => {
+	return res.sendFile("/build/index.html", { root: __dirname + "/" });
+});
+
+app.listen(PORT, () => {
+	console.log(
+		`The server is up and running. The app is listening on Port ${PORT}`,
+	);
+});
---
+++ server/routes/users.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+module.exports = router;
+
+//*Need three routes for register, login and verify.
+//*Need a function to verify data.
