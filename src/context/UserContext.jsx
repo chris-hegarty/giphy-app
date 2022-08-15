52b563cdc5e20053473187598f701474b2eaa81c
chegarty74 <chegarty74@yahoo.com>
2022-08-15 12:10:10 -0500

    Add register function to UserContext.

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,10 +1,34 @@
 
 import React, { useState, createContext, useCallback } from "react";
+//*need to add axios here:
+import axios from "axios";
 export const UserContext = createContext(null);
 
 export function UserProvider(props){
     //state and functionality goes here:
     const [loggedInUser, setLoggedInUser] = useState("Chris");
+    //*add and update functions:
+    //*Here is a new register function.
+    //* Your RegisterPage.jsx will call this function.
+    //*The request goes to the route set up in route file, then to model, then response back from model to route
+
+    const register = useCallback( async (username, password)=> {
+        try{
+            const response = await axios.put("/api/users/register", {
+                username,
+                password,
+            });
+            if(response.data.success){
+                console.log("Registration successful");
+            } else {
+                console.log(response.data.error);
+            }
+        } catch(err) {
+            console.log(err);
+        }
+    },[]);
+
+    
 
     const login = useCallback(
         (username) => {
